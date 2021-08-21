4123c2142c46326f00854394d7620891daa0187a
Alexandre <alexandre.jourdain28@gmail.com>
2021-08-21 12:22:09 +0200

    modif

--- front-end/js/product.js
+++ front-end/js/product.js
@@ -1,4 +1,3 @@
-
 const searchParams = new URLSearchParams(window.location.search);
 const productId = searchParams.get("id");
 
@@ -9,10 +8,11 @@ fetch("http://localhost:3000/api/cameras/" + productId)
 		}
 	})
 	.then((jsonArticle) => {
-			let article = new Article(jsonArticle);
-			document.querySelector(".camera_fav").innerHTML += `
+		let article = new Article(jsonArticle);
+		document.querySelector(".camera_fav").innerHTML += `
 				<article>
                 <a href="products.html?id=${article._id}">
+				<H1>Modele ${article.name}</H1>
                     <div class="position_img_article">
                         <img class="img_article" src="${article.imageUrl}" alt="">
                     </div>
@@ -20,22 +20,35 @@ fetch("http://localhost:3000/api/cameras/" + productId)
                         <div class="position_camera">
                             <i class=" icon_cam fas fa-camera-retro"></i>
                         </div>
-                        <div class="text-article">
+                          <div class="text-article">
                             <p> modele${article.name}</p>
-							<p>${article.price } € </p>
-                        </div>
-                    </div>	
-                </a>
-            </article>
+							<p>${article.price} € </p>
+                          </div>
+						   </a>
+						   <div class="position_form">
+            <div class="position_label">
+                <label for="lentille-select"></label>
+                <select name="" id="">
+                    <option value="">Selectionner votre lentille</option>
+                </select>
+            </div>   
+        </div>
+							
+						</article>
 																`;
 
-
-                        // for (let option of article.lenses) {
-                        // document.querySelector (".lentille-select").innerHtml+=
-                        // <option value="lentille">${option}</option>
-                        // }                                      
-		}
+		// for (let option of article.lenses) {
+		// document.querySelector (".lentille-select").innerHtml+=
+		// <option value="lentille">${option}</option>
+		// }
+	}
+	// for (let option of article.lenses) {
+		// document.querySelector (".lentille-select").innerHtml+=
+		// <option value="lentille">${option}</option>
+		// }
+	
 	)
+	
 	.then(function (cameras) {
 		console.log(cameras);
 		cameras.forEach((camera) => {
@@ -43,4 +56,3 @@ fetch("http://localhost:3000/api/cameras/" + productId)
 		});
 	})
 	.catch(function (error) {});
-    
